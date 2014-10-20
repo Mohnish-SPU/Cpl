98d4f13f6f577780467a6e53abd3d692f586ad55
Mohnish SPU <mspu1996@gmail.com>
2014-10-20 16:38:29 +0530

    Final code for auto indentation wid out special for() or  cases

--- cs13b019_lab8/Q2/main.c
+++ cs13b019_lab8/Q2/main.c
@@ -8,6 +8,108 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define TAB "\t"
+
+char *clearFile(FILE *read, FILE *write); //Removes all tab spaces and newline chars in given txt file
+void indentLine(char *line, FILE *write);//Adds tabs and newline
+
+char *clearFile(FILE *read, FILE *write)
+{
+	char* line = NULL;
+	char* inter;
+	char* line_final = (char*)malloc(1);
+	line_final[0] = '\0';
+	int lenstr = 0;
+	
+	size_t len = 0; 
+	ssize_t read1;
+	int i, j = 0;
+	while ((read1 = getline(&line, &len, read)) != -1)
+	{
+		
+		if(line[0] != '#')
+		{
+			inter = (char*)malloc(strlen(line));
+			int i = 0;
+			int j = 0;
+			while(line[i] != '\n')
+			{
+				if(line[i] != '\t')
+				{
+					inter[j] = line[i];
+					j++;
+				}	
+				i++;
+			}
+			
+			inter[j] = '\0';
+			lenstr = strlen(line_final);
+			line_final = (char*)realloc(line_final,lenstr+strlen(inter)+1);
+			line_final = strcat(line_final, inter);
+		}
+		else
+		{
+			
+			fprintf(write,"%s",line);
+		}
+	}
+	return line_final;
+}
+
+void indentLine(char* line, FILE *write)
+{
+	int i, j;
+	i = 0;
+	int tabCount = 0;
+	while(line[i] != '\0')
+	{
+		if(line[i] == '{')
+		{	
+			fprintf(write,"\n");
+			for(j = 0;j < tabCount;j++)
+			{
+				fprintf(write,"\t");	
+			}
+			fprintf(write,"{\n");
+			tabCount += 1;
+			for(j = 0;j < tabCount;j++)
+			{
+				fprintf(write,"\t");	
+			}
+		}
+		else if(line[i] == '}')
+		{
+			fprintf(write,"\n");
+			tabCount -= 1;
+			for(j = 0;j < tabCount;j++)
+			{
+				fprintf(write,"\t");	
+			}
+			
+			fprintf(write,"}\n");
+			for(j = 0;j < tabCount;j++)
+			{
+				fprintf(write,"\t");	
+			}
+			
+		}
+		else if(line[i] == ';')
+		{
+			if(line[i+1] != '}')
+				fprintf(write,";\n");
+			for(j = 0;j < tabCount;j++)
+			{
+				fprintf(write,"\t");	
+			}
+			
+		}else
+		{
+			fprintf(write,"%c",line[i]);
+		}
+		i++;
+	}
+}
+
 int
 main(
 	int argc,
@@ -34,6 +136,10 @@ main(
 	if( argc == 3)
 	{
 		
+		char* line = clearFile(read, write);
+		//fflush(write);
+		//fclose(write);
+		indentLine(line, write);	
 	} 
 	else
 	{
